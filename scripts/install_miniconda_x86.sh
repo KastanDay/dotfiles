4902fbaca28bddb96cc2d8dc0a019f5d496783a7
Kastan Day <kastanvday@gmail.com>
2025-02-25 11:13:20 -0800

    Remove depricated Mamba, switch to regular MiniConda

--- scripts/install_miniconda_x86.sh
+++ scripts/install_miniconda_x86.sh
@@ -1,10 +1,11 @@
 install_miniconda_x86 () {
-mkdir -p ~/utils/miniconda3
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh -O ~/utils/miniconda3/miniconda.sh
-bash ~/utils/miniconda3/miniconda.sh -b -u -p ~/utils/miniconda3
-rm -rf ~/utils/miniconda3/miniconda.sh
-~/utils/miniconda3/bin/mamba init zsh
-source ~/.zshrc
+    mkdir -p ~/utils/miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/utils/miniconda3/miniconda.sh
+    bash ~/utils/miniconda3/miniconda.sh -b -u -p ~/utils/miniconda3
+    rm -rf ~/utils/miniconda3/miniconda.sh
+    source ~/utils/miniconda3/bin/activate
+    ~/utils/miniconda3/bin/conda init --all
+    source ~/.zshrc
 }
 
 # run it
